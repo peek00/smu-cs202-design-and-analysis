200349fd50da60886a3fb3faae0336dea2a4e08a
xun <xunyi.work@gmail.com>
2024-03-05 10:12:47 +0800

    crying

--- assignment02/A2Q2_OnlineJudge.py
+++ assignment02/A2Q2_OnlineJudge.py
@@ -13,72 +13,37 @@ def LCMS(a, b):
     # return answer
 
     commoners = get_longest_common_subsequence(a, b)
-    print("Commoners: ", len(commoners))
-    print(commoners)
+    # print("Commoners: ", len(commoners))
+    # print(commoners)
     answer = do_the_mountain_thing_third_time(commoners)
+    # answer = do_the_mountain_thing_correctly_this_time(commoners)
     return answer
-#
 
 # def get_common_elements_in_relative_order(a, b):
-#     """
-#     Intuition here is to maintain a counter and a unique "trailing" set 
-#     for both a and b that consists of numbers before the current counter.
-
-#     If a[i] exists inside the trailing set of b, this means that there is
-#     a match of a[i] and a number earlier in the list of b. We find the index 
-#     of the element in b from the previous counter (if it exists, else 0), and then
-#     set that as the previous counter before clearing a_set. 
-
-#     We clear a_set because the ones before a_set are no longer in relative order. 
-#     However, this means that we have to re search from the index of b element
-
-#     """
-
-
-# def get_common_elements_in_relative_order(a, b):
-#     if len(a) > len(b):
-#         a, b = b, a
-#     dp = [[0]* (len(a) + 1 ) for _ in range(len(b) + 1)]
-#     commoners = []
-#     for i in range(len(b) - 1, -1, -1):
-#         for j in range(len(a) - 1, -1, -1):
-#             if a[j] == b[i]:
-#                 dp[i][j] = dp[i+1][j+1] + 1
-#                 commoners.append(a[j])
-#                 break
-#             else:
-#                 dp[i][j] = max(dp[i+1][j], dp[i][j+1])
-#     return commoners[::-1]
-
-def get_common_elements_in_relative_order(a, b):
-    # Create sets of both
-    a_set = set(a)
-    b_set = set(b)
-    for i in range(len(a)):
-        if a[i] not in b_set:
-            a[i] = None
-    for i in range(len(b)):
-        if b[i] not in a_set:
-            b[i] = None
-
-    print(a)
-    print(b)
-    return []
-
+#     # Create sets of both
+#     a_set = set(a)
+#     b_set = set(b)
+#     for i in range(len(a)):
+#         if a[i] not in b_set:
+#             a[i] = None
+#     for i in range(len(b)):
+#         if b[i] not in a_set:
+#             b[i] = None
+#     return []
 
 def do_the_mountain_thing_third_time(commoners):
-    strictly_up = lengthOfLIS(commoners)
-    strictly_down = lengthOfLDS(commoners)
-    print(f"Commoners: {commoners}")
-    print(f"Strictly up: {strictly_up}")
-    print(f"Strictly down: {strictly_down}")
+    strictly_up = get_longest_increasing_sequence(commoners)
+    strictly_down = get_longest_decreasing_sequence(commoners)
+    # print(f"Commoners: {commoners}")
+    # print(f"Strictly up: {strictly_up}")
+    # print(f"Strictly down: {strictly_down}")
     max_sum = 0
     for num1, num2 in zip(strictly_up, strictly_down):
         max_sum = max(max_sum, num1 + num2)
   
     return max_sum + 1
 
-def lengthOfLDS(nums) -> int:
+def get_longest_decreasing_sequence(nums):
     if not nums:
         return []
 
@@ -92,7 +57,7 @@ def lengthOfLDS(nums) -> int:
 
     return dp 
 
-def lengthOfLIS(nums) -> int:
+def get_longest_increasing_sequence(nums):
     if not nums:
         return 0
     
@@ -105,35 +70,71 @@ def lengthOfLIS(nums) -> int:
                 dp[i] = max(dp[i], dp[j] + 1)
 
     return dp
+
 def get_longest_common_subsequence(a,b):
-    # Solve this using bottom up dynammic programming
-    # Created 2D matrix with +1 length for both A and B
-    # Start from top left, if it matches, move diagonal.
-
-    dp = [[0] * (len(a) + 1) for _ in range(len(b) + 1) ] 
-    prev_indices = [[None] * (len(a) + 1) for _ in range(len(b) + 1)]
-
+    if len(a) < len(b):
+        a, b = b, a
+    dp = [[0] * (len(a) + 1) for _ in range(len(b) + 1) ]
+    
+    # Construct common subsequence length
     for i in range(1, len(b) + 1):
-        dp[i % 2][0] = 0
         for j in range(1, len(a) + 1):
             if a[j - 1] == b[i - 1]:
-                dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1
-                prev_indices[i][j] = (i - 1, j - 1)
+                dp[i][j] = dp[i - 1][j - 1] + 1
             else:
-                if dp[(i - 1) % 2][j] > dp[i % 2][j - 1]:
-                    dp[i % 2][j] = dp[(i - 1) % 2][j]
-                    prev_indices[i][j] = (i - 1, j)
-                else:
-                    dp[i % 2][j] = dp[i % 2][j - 1]
-                    prev_indices[i][j] = (i, j - 1)
-    # Retrieve path
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    # Retrieve length of common subsequence from DP table
+    # Go from bottom right to top left
+    # 
     commoners = []
-    i, j = len(b), len(a)
-    while i > 0 and j > 0:
-        if a[j - 1] == b[i - 1]:
-            commoners.append(a[j - 1])
-        i, j = prev_indices[i][j]
+    # Both index of string and dp table
+    p1 = len(a)
+    p2 = len(b)
+    while p1 > 0 and p2 > 0:
+        # Checks if the string at index matches
+        if a[p1 - 1] == b[p2 - 1]: 
+            commoners.append(a[p1 - 1])
+            p1 -= 1
+            p2 -= 1
+        # Checks in DP table if it is greater left or upwards
+        # If there is an increase, it means there is a common character in both string
+        elif dp[p2][p1 - 1] > dp[p2 - 1][p1]:
+            p1 -= 1
+        else:
+            p2 -= 1
+
     return commoners[::-1]
+    
+# def get_longest_common_subsequence(a,b):
+#     # Solve this using bottom up dynammic programming
+#     # Created 2D matrix with +1 length for both A and B
+#     # Start from top left, if it matches, move diagonal.
+
+#     dp = [[0] * (len(a) + 1) for _ in range(len(b) + 1) ] 
+#     prev_indices = [[None] * (len(a) + 1) for _ in range(len(b) + 1)]
+
+#     for i in range(1, len(b) + 1):
+#         dp[i % 2][0] = 0
+#         for j in range(1, len(a) + 1):
+#             if a[j - 1] == b[i - 1]:
+#                 dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1
+#                 prev_indices[i][j] = (i - 1, j - 1)
+#             else:
+#                 if dp[(i - 1) % 2][j] > dp[i % 2][j - 1]:
+#                     dp[i % 2][j] = dp[(i - 1) % 2][j]
+#                     prev_indices[i][j] = (i - 1, j)
+#                 else:
+#                     dp[i % 2][j] = dp[i % 2][j - 1]
+#                     prev_indices[i][j] = (i, j - 1)
+#     # Retrieve path
+#     commoners = []
+#     i, j = len(b), len(a)
+#     while i > 0 and j > 0:
+#         if a[j - 1] == b[i - 1]:
+#             commoners.append(a[j - 1])
+#         i, j = prev_indices[i][j]
+#     return commoners[::-1]
 
 def do_the_mountain_thing_correctly_this_time(commoners):
     """
@@ -183,21 +184,17 @@ def do_the_mountain_thing_correctly_this_time(commoners):
         if max_sum == num1 + num2:
             _ = count
     # print("maxy", maxy.index(max_sum), commoners[_])
-    # print(maxy)
+    print(max_sum + 1)
+
     return max_sum + 1
 
 
 num_pair = int(sys.stdin.readline())
 
 for _ in range(num_pair):
-    print("Case #", _+1, ": ", sep='', end='')
+    # print("Case #", _+1, ": ", sep='', end='')
     a = [int(s) for s in sys.stdin.readline().split()]
     b = [int(s) for s in sys.stdin.readline().split()]
     print(LCMS(a, b))
-    print()
-
-print("test")
-_ = [1,2,3,4,3,4,2,5]
-print(lengthOfLIS(_))
-
+    # print()
 
